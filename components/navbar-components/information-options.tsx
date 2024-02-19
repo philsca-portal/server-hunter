f2837a90cc4ee0bdd466d27c0cec02c572a228a2
Bryan Revilla Tabajonda <tabajondabryan7@gmail.com>
2024-02-20 02:23:35 +0800

    white

--- components/navbar-components/information-options.tsx
+++ components/navbar-components/information-options.tsx
@@ -11,7 +11,7 @@ const InformationOptions: React.FC<InformationOptionsProps> = ({
     clicked
 }) => {
     return(
-        <div className={`absolute z-10 mt-2 border border-[#e87b73] text-[#e87b73] rounded-3xl ${clicked ? ' animate-pop-up-animation' : 'hidden'} `}>
+        <div className={`absolute z-10 mt-2 border bg-white border-[#e87b73] text-[#e87b73] rounded-3xl ${clicked ? ' animate-pop-up-animation' : 'hidden'} `}>
             <div className="flex flex-col text-sm p-4">
                 <Link className="py-3 px-4 hover:text-white hover:bg-[#e87b73] rounded-2xl transition" href={'/providers'}>
                     <div className="flex item-center">
